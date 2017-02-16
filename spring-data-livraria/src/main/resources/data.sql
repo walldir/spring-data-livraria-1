0415fa24615796394e636314262e6101de9f489e
adfor.glaydson <glaydson.sousa@bcb.gov.br>
2017-02-16 11:38:03 -0300

    Incluindo script sql para carga de dados na tabela de livros

---
+++ spring-data-livraria/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into tb_livro(livro_id, titulo, data_publicacao, numero_paginas, preco) values (1,'Sobre Mouses e Homens', '1954-11-08', 100, 11.00);
+insert into tb_livro(livro_id, titulo, data_publicacao, numero_paginas, preco) values (2,'Um Estranho no Ninho', '1973-11-08', 200, 15.00);
+insert into tb_livro(livro_id, titulo, data_publicacao, numero_paginas, preco) values (3,'Por Quem os Sinos Dobram', '1932-11-08', 100, 13.00);
+insert into tb_livro(livro_id, titulo, data_publicacao, numero_paginas, preco) values (4,'Guerra e Paz', '1955-11-08', 140, 15.00);
+insert into tb_livro(livro_id, titulo, data_publicacao, numero_paginas, preco) values (5,'As Vinhas da Ira', '1955-11-08', 300, 16.00);
+insert into tb_livro(livro_id, titulo, data_publicacao, numero_paginas, preco) values (6,'Padrões de Design', '1996-11-08', 180, 14.00);
+insert into tb_livro(livro_id, titulo, data_publicacao, numero_paginas, preco) values (7,'Um Conto de Duas Cidades', '1943-11-08', 400, 15.00);
